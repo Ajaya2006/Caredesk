a6f3c8f10dcde54b84f84aa36cb7c353ef8f9da9
Ajaya Kumar Sahoo <sahooajayaaks12345@gmail.com>
2026-09-02 09:02:07 +0530

    Fix: Update AuthInitializer import to use correct case (Components)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import { RouterProvider } from 'react-router-dom';
 import { QueryProvider } from './providers/QueryProvider';
 import { ThemeProvider } from './providers/ThemeProvider';
 import { ToastProvider } from './providers/ToastProvider';
-import { AuthInitializer } from './components/AuthInitializer';
+import { AuthInitializer } from './Components/AuthInitializer';
 import { router } from './routes';
 import './styles/globals.css';
 import './styles/theme.css';
